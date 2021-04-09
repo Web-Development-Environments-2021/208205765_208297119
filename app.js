f83bcab84d37b7a2500641313ca6ac0d17585e26
ronny5423 <ronny54@gmail.com>
2021-04-09 23:20:35 +0300

    finish regestration

--- app.js
+++ app.js
@@ -1,183 +1,18 @@
-var context;
-var shape = new Object();
-var board;
-var score;
-var pac_color;
-var start_time;
-var time_elapsed;
-var interval;
 
-$(document).ready(function() {
-	context = canvas.getContext("2d");
-	Start();
-});
-
-function Start() {
-	board = new Array();
-	score = 0;
-	pac_color = "yellow";
-	var cnt = 100;
-	var food_remain = 50;
-	var pacman_remain = 1;
-	start_time = new Date();
-	for (var i = 0; i < 10; i++) {
-		board[i] = new Array();
-		//put obstacles in (i=3,j=3) and (i=3,j=4) and (i=3,j=5), (i=6,j=1) and (i=6,j=2)
-		for (var j = 0; j < 10; j++) {
-			if (
-				(i == 3 && j == 3) ||
-				(i == 3 && j == 4) ||
-				(i == 3 && j == 5) ||
-				(i == 6 && j == 1) ||
-				(i == 6 && j == 2)
-			) {
-				board[i][j] = 4;
-			} else {
-				var randomNum = Math.random();
-				if (randomNum <= (1.0 * food_remain) / cnt) {
-					food_remain--;
-					board[i][j] = 1;
-				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
-					shape.i = i;
-					shape.j = j;
-					pacman_remain--;
-					board[i][j] = 2;
-				} else {
-					board[i][j] = 0;
-				}
-				cnt--;
-			}
-		}
-	}
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
-	}
-	keysDown = {};
-	addEventListener(
-		"keydown",
-		function(e) {
-			keysDown[e.keyCode] = true;
-		},
-		false
-	);
-	addEventListener(
-		"keyup",
-		function(e) {
-			keysDown[e.keyCode] = false;
-		},
-		false
-	);
-	interval = setInterval(UpdatePosition, 250);
-}
-
-function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 9 + 1);
-	var j = Math.floor(Math.random() * 9 + 1);
-	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 9 + 1);
-		j = Math.floor(Math.random() * 9 + 1);
-	}
-	return [i, j];
-}
-
-function GetKeyPressed() {
-	if (keysDown[38]) {
-		return 1;
-	}
-	if (keysDown[40]) {
-		return 2;
-	}
-	if (keysDown[37]) {
-		return 3;
-	}
-	if (keysDown[39]) {
-		return 4;
-	}
-}
-
-function Draw() {
-	canvas.width = canvas.width; //clean board
-	lblScore.value = score;
-	lblTime.value = time_elapsed;
-	for (var i = 0; i < 10; i++) {
-		for (var j = 0; j < 10; j++) {
-			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
-			if (board[i][j] == 2) {
-				context.beginPath();
-				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
-				context.lineTo(center.x, center.y);
-				context.fillStyle = pac_color; //color
-				context.fill();
-				context.beginPath();
-				context.arc(center.x + 5, center.y - 15, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 1) {
-				context.beginPath();
-				context.arc(center.x, center.y, 15, 0, 2 * Math.PI); // circle
-				context.fillStyle = "black"; //color
-				context.fill();
-			} else if (board[i][j] == 4) {
-				context.beginPath();
-				context.rect(center.x - 30, center.y - 30, 60, 60);
-				context.fillStyle = "grey"; //color
-				context.fill();
-			}
-		}
-	}
-}
-
-function UpdatePosition() {
-	board[shape.i][shape.j] = 0;
-	var x = GetKeyPressed();
-	if (x == 1) {
-		if (shape.j > 0 && board[shape.i][shape.j - 1] != 4) {
-			shape.j--;
-		}
-	}
-	if (x == 2) {
-		if (shape.j < 9 && board[shape.i][shape.j + 1] != 4) {
-			shape.j++;
-		}
-	}
-	if (x == 3) {
-		if (shape.i > 0 && board[shape.i - 1][shape.j] != 4) {
-			shape.i--;
-		}
-	}
-	if (x == 4) {
-		if (shape.i < 9 && board[shape.i + 1][shape.j] != 4) {
-			shape.i++;
-		}
-	}
-	if (board[shape.i][shape.j] == 1) {
-		score++;
-	}
-	board[shape.i][shape.j] = 2;
-	var currentTime = new Date();
-	time_elapsed = (currentTime - start_time) / 1000;
-	if (score >= 20 && time_elapsed <= 10) {
-		pac_color = "green";
-	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
-	} else {
-		Draw();
-	}
-}
+let usersDict={"k":["k","","",""]};
+let currentDisplayedDiv="#welcomeContainer";
 
 function validateDataAfterRegistretion(){
-	userName=$("#userName").val();
+	let userName=$('#userName').val();
 	if(userName==""){
 		alert("Empty userName!");
 		return;
 	}
-	password=$("#password").val();
+	if (userName in usersDict){
+		alert("Please choose another userName");
+		return;
+	}
+	let password=$("#password").val();
 	if(password==""){
 		alert("Please fill the password field");
 		return;
@@ -186,7 +21,7 @@ function validateDataAfterRegistretion(){
 		alert("Please fill the password again");
 		return;
 	}
-	fullName=$("#fullName").val();
+	let fullName=$("#fullName").val();
 	if(fullName==""){
 		alert("Please fill full name");
 		return;
@@ -195,6 +30,22 @@ function validateDataAfterRegistretion(){
 		alert("Please refill the full name again");
 		return;
 	}
+	let email=$("#email").val();
+	if(email==""){
+		alert("Please enter email!");
+		return;
+	}
+	if(!checkEmail(email)){
+		alert("Please enter valid email");
+		return;
+	}
+	let birthDate=$("#birthDate").val();
+	usersDict[userName]=[password,fullName,email,birthDate];
+}
+
+function checkEmail(email){
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function checkPassword(password){
@@ -234,4 +85,23 @@ function checkFullName(fullName){
 		}
 	}
 	return true;
+}
+
+function register(){
+	switchDivs("#registerDiv","block");	
+}
+
+function showWelcomeScreen(){
+	switchDivs("#welcomeContainer","flex");
+}
+
+/**
+ * This method switch between the current screen and new screen
+ * @param {*div} newDivToSwitchTo the new div to show 
+ * @param {string} display  a string value of how display the div(flex,inline etc...)
+ */
+function switchDivs(newDivToSwitchTo,display){
+	$(currentDisplayedDiv).css("display","none");
+	currentDisplayedDiv=newDivToSwitchTo;
+	$(newDivToSwitchTo).css("display",display);
 }
