95986d76499c6169f1795ca7e8e84082647828fd
ronny5423 <ronny54@gmail.com>
2021-04-10 22:07:15 +0300

    add settings window

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 let usersDict={"k":["k","","",""]};
 let currentDisplayedDiv="#welcomeContainer";
+let keys={"Up":"","Down":"","Left":"","Right":""};
+let validSettingsData=true;
+let ballsNumber=0;
+let timeOfGame=0;
+let monstersNumber=0;
+let ballsColors={};
 
 function validateDataAfterRegistretion(){
 	let userName=$('#userName').val();
@@ -14,7 +20,7 @@ function validateDataAfterRegistretion(){
 	}
 	let password=$("#password").val();
 	if(password==""){
-		alert("Please fill the password field");
+		alert("password should contain at least 1 letter and 1 digit");
 		return;
 	}
 	if(!checkPassword(password)){
@@ -27,7 +33,7 @@ function validateDataAfterRegistretion(){
 		return;
 	}
 	if(!checkFullName(fullName)){
-		alert("Please refill the full name again");
+		alert("full name should contain only letters");
 		return;
 	}
 	let email=$("#email").val();
@@ -41,6 +47,7 @@ function validateDataAfterRegistretion(){
 	}
 	let birthDate=$("#birthDate").val();
 	usersDict[userName]=[password,fullName,email,birthDate];
+	showSettings();
 }
 
 function checkEmail(email){
@@ -80,7 +87,7 @@ function checkPassword(password){
 function checkFullName(fullName){
 	for(let i=0;i<fullName.length;i++){
 		let asciiValue=fullName.charAt(i).charCodeAt(0);
-		if(asciiValue>=48 && asciiValue<=57){
+		if(asciiValue>=123 || asciiValue<=96){
 			return false;
 		}
 	}
@@ -95,6 +102,13 @@ function showWelcomeScreen(){
 	switchDivs("#welcomeContainer","flex");
 }
 
+function showSettings(){
+	$("#gameAndSettingsDiv").css("display","flex");
+	$("#settingsDiv").css("display","block");
+	$(currentDisplayedDiv).css("display","none");
+	currentDisplayedDiv="#settingsDiv";
+}
+
 /**
  * This method switch between the current screen and new screen
  * @param {*div} newDivToSwitchTo the new div to show 
@@ -105,3 +119,137 @@ function switchDivs(newDivToSwitchTo,display){
 	currentDisplayedDiv=newDivToSwitchTo;
 	$(newDivToSwitchTo).css("display",display);
 }
+
+/**
+ * This function gets the keys the user press for movment and show them to the user
+ */
+$(document).ready(function(){
+	setKeyPressed("#keyUp");
+	setKeyPressed("#keyDown");
+	setKeyPressed("#keyLeft");
+	setKeyPressed("#keyRight");
+});
+
+/**
+ * This function sets a listener for each user input key to show the user the key he pressed
+ * @param {*} keyIdToSet key input id to set the listener to
+ */
+function setKeyPressed(keyIdToSet){
+	$(keyIdToSet).keydown(function(event){
+		$(keyIdToSet).attr("placeholder",event.key);
+		$(keyIdToSet).val(event.key);
+	});
+}
+
+function validateSettings(){
+	$(".errorRow").css("display","none");
+	validSettingsData=true;
+	let keyup=$("#keyUp").val();
+	let keyDown=$("#keyDown").val();
+	let keyLeft=$("#keyLeft").val();
+	let keyRight=$("#keyRight").val();
+	let numberOfBalls=$("#numberOfBalls").val();
+	let fivePointsBallColor=$("#fivePointsColorPicker").val();
+	let fifteenPointsColorPicker=$("#fifteenPointsColorPicker").val();
+	let twentyFivePointsColorPicker=$("#twentyFivePointsColorPicker").val();
+	let gameTime=$("#gameTimeInput").val();
+	let numberOfMonsters=$("#monstersPicker").val();
+	checkKeysSettings(keyup,keyDown,keyLeft,keyRight);
+	checkNumericValuesSettings(numberOfBalls,gameTime,numberOfMonsters);
+	if(validSettingsData){
+		ballsColors["5"]=fivePointsBallColor;
+		ballsColors["15"]=fifteenPointsColorPicker;
+		ballsColors["25"]=twentyFivePointsColorPicker;
+		$("#gameDiv").css("display","flex");
+	}
+}
+
+function checkNumericValuesSettings(numberOfBalls,gameTime,numberOfMonsters){
+	if(checkSingleNumericValue(numberOfBalls,50,90,"#errorBallsNumber","Number of balls must be a number","Number of balls must be between 50 to 90")){
+		ballsNumber=parseInt(numberOfBalls);
+	}
+	if(checkSingleNumericValue(gameTime,60,0,"#gameTimeError","Game time must be at least 60 seconds!","")){
+		timeOfGame=parseInt(gameTime);
+	}
+	if(checkSingleNumericValue(numberOfMonsters,1,4,"#monstersNumberError","Monsters number must be a number","Monsters number must be between 1 to 4")){
+		monstersNumber=parseInt(numberOfMonsters);
+	}
+}
+
+function checkSingleNumericValue(numericValue,min,max,rowId,message1,message2){
+	if(numericValue==""){
+		$(rowId+" td").html("Please enter value");
+		$(rowId).css("display","block");
+		validSettingsData=false;
+		return false;
+	}
+	if(isNaN(numericValue)){
+		$(rowId+" td").html(message1);
+		$(rowId).css("display","block");
+		validSettingsData=false;
+		return false;
+	}
+	let intNumericValue=parseInt(numericValue);
+	if(max==0){
+		if(intNumericValue<min){
+			$(rowId+" td").html(message2);
+			$(rowId).css("display","block");
+			validSettingsData=false;
+			return false;
+		}
+		return true;
+	}
+	if(intNumericValue<min || intNumericValue>max){
+		$(rowId+" td").html(message2);
+		$(rowId).css("display","block");
+		validSettingsData=false;
+		return false;
+	}
+	return true;
+}
+
+function checkKeysSettings(keyUp,keyDown,keyLeft,keyRight){
+	showKeyErrorMessage(keyUp,"#errorKeyUp","Up");
+	showKeyErrorMessage(keyDown,"#errorKeyDown","Down");
+	showKeyErrorMessage(keyLeft,"#errorLeftKey","Left");
+	showKeyErrorMessage(keyRight,"#rightKeyError","Right");
+}
+
+function showKeyErrorMessage(keyToCheck,rowId,direction){
+	if(keyToCheck==""){
+		$(rowId).css("display","block");
+		validSettingsData=false;
+		}
+	else{
+		keys[direction]=keyToCheck;
+	}
+}
+
+function generateRandomSettings(){
+	keys["Up"]="ArrowUp";
+	keys["Down"]="ArrowDown";
+	keys["Left"]="ArrowLeft";
+	keys["Right"]="ArrowRight";
+	timeOfGame=60;
+	monstersNumber=3;
+	ballsNumber=60;
+	ballsColors["5"]="red";
+	ballsColors["15"]="blue;"
+	ballsColors["25"]="green";
+	updateSettingsValues();
+	$("#gameDiv").css("display","flex");
+}
+
+function updateSettingsValues(){
+	$("#keyUp").val("ArrowUp");
+	$("#keyDown").val("ArrowDown");
+	$("#keyLeft").val("ArrowLeft");
+	$("#keyRight").val("ArrowRight");
+	$("#numberOfBalls").val("60");
+	$("#fivePointsColorPicker").val("#FF0000");
+	$("#fifteenPointsColorPicker").val("#0040FF");
+	$("#twentyFivePointsColorPicker").val("#00FF08");
+	$("#gameTimeInput").val("60");
+	$("#monstersPicker").val("60");
+}
+
