3e28ce5e4f65f7db5a1986964890bd606f3598bb
tomer99a <69591504+tomer99a@users.noreply.github.com>
2021-04-16 12:04:22 +0300

    add Medication and more

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ let countCandy = 0;
 let radiusCandys = {5:12, 15:8, 25:4};
 let backgroundSong=new Audio("Beethoven-Symphony5.mp3");
 let lives=5;
+let medicationX;
+let medicationY;
+let medicationTime;
+let medicationTimeTolive = 100;
+let medicationDifficulty = 2;
 
 $(document).ready(function(){
 	ctx=document.getElementById("myCanvas").getContext("2d");
@@ -321,14 +326,14 @@ function generateRandomSettings(){
 	updateSettingsValues();
 	}
 
-	function getRandomColor() {
-		let letters = '0123456789ABCDEF';
-		let color = '#';
-		for (let i = 0; i < 6; i++) {
-		  color += letters[Math.floor(Math.random() * 16)];
-		}
-		return color;
-	  }
+function getRandomColor() {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
 
 function updateSettingsValues(){
 	$("#keyUp").val("ArrowUp");
@@ -477,43 +482,28 @@ function setWallsOnBoard(){
 }
 
 function setBallsOnBoard(){
-	let numberOfFiveBalls=parseInt(ballsNumber*0.6);
-	let numberOfFifteen=parseInt(ballsNumber*0.3);
-	let numberOfTwentyFive=ballsNumber-numberOfFiveBalls-numberOfFifteen;
-	let arrayOfCounters=[numberOfFiveBalls,numberOfFifteen,numberOfTwentyFive];
-	let counter=ballsNumber;
+	let numberOfFiveBalls = parseInt(ballsNumber * 0.6);
+	let numberOfFifteen = parseInt(ballsNumber * 0.3);
+	let numberOfTwentyFive = ballsNumber - numberOfFiveBalls - numberOfFifteen;
+	let arrayOfCounters = [numberOfFiveBalls, numberOfFifteen, numberOfTwentyFive];
+	let counter = ballsNumber;
+	let numToPrice = {0:5, 1:15, 2:25};
 	(function() {
-		for(let i=0;i<board.length;i++){
-		for(let j=0;j<board[0].length;j++){
+		while(counter > 0)
+		{
+			let i = Math.floor(Math.random()*board.length);
+			let j = Math.floor(Math.random()*board[0].length);
 			if(board[i][j]==0){
-				let ball=Math.floor(Math.random()*3);
+				let ball = Math.floor(Math.random()*3);
 				while(arrayOfCounters[ball]==0){
 					ball=Math.floor(Math.random()*3);
 				}
-				switch (ball) {
-					case 0:
-						board[i][j]=5;
-						arrayOfCounters[0]--;
-						counter--;
-						break;
-					case 1:
-						board[i][j]=15;
-						arrayOfCounters[1]--;
-						counter--;
-						break;
-					case 2:
-						board[i][j]=25;
-						arrayOfCounters[2]--;
-						counter--;
-						break;	
-					
-				}
-				if(counter==0){
-					return;
-				}
+				board[i][j] = numToPrice[ball];
+				arrayOfCounters[ball]--;
+				counter--;
 			}
 		}
-	}})();
+	})();
 }
 
 
@@ -564,7 +554,7 @@ function createDirections(){
 
 function startGame(){
 	$("#userNameToShow").html(loggedUser);
-	score=0;
+	score = 0;
 	$("#scoreLabel").html("0");
 	backgroundSong.loop=true;
 	//backgroundSong.play();
@@ -577,6 +567,9 @@ function startGame(){
 	gameIntervals.push(intervalTimer);
 	setGameTimer();
 	drawLives();
+	pacSpeed = 4;
+	medicationDifficulty = 2;
+	initMedicationPosition(medicationDifficulty);
 }
 
 // terminate interval timer
@@ -586,10 +579,6 @@ function stopTimer()
 } 
 
 function main(){
-	if (angle > 0.188 || angle < 0.0001)
-		swicthAngle *= -1;
-	angle = swicthAngle * 0.02 + angle;
-
 	drawMap();
 	drawPacman();
 	drawCherry();
@@ -599,16 +588,21 @@ function main(){
 	}
 	drawGhosts();
 	changeGhostsLocations();
+	drawMedication();
+
+	if (angle > 0.188 || angle < 0.0001)
+		swicthAngle *= -1;
+	angle = swicthAngle * 0.02 + angle;
 }
 
 function drawCherry(){
 	cherryX += cherrySpeedX;
 	cherryY += cherrySpeedY;
 
-	if (cherryX+50 > 900 || cherryX < 0)
+	if (cherryX+50 >= 900 || cherryX <= 0)
 		cherrySpeedX *= -1;
 	
-	if (cherryY+40 > 450 || cherryY < 0)
+	if (cherryY+40 >= 450 || cherryY <= 0)
 		cherrySpeedY *= -1;
 
 	let img = new Image();
@@ -637,6 +631,25 @@ function drawMap(){
 			}				
 		}
 	}
+}
+
+function drawMedication(){
+	if (medicationTime <= medicationTimeTolive){
+		let img = new Image();
+		img.src = "Img/drug.jpg";
+		ctx.drawImage(img, medicationX, medicationY, 50, 40);
+		
+		if (pacmanContact(medicationX, medicationY, 50, 40)){
+			changeLives(1);
+			medicationDifficulty++;
+			initMedicationPosition(medicationDifficulty);
+			pacSpeed++;			
+		}
+	}
+	if (medicationTime <= 0){
+		initMedicationPosition(medicationDifficulty);
+	}
+	medicationTime--;
 }
 
 function drawPacman(){
@@ -716,15 +729,18 @@ document.addEventListener('keydown', function (event) {
 			pacX += pacSpeed;
 			touchCandy();
 		}
-	}	
-  });
+	}
+});
 
 function checkWall(x, y){
-	if (x > 900 || x < 0)
+	if (x >= 900 || x < 0)
 		return true;
-	if (y > 450 || y < 0)
+	if (y >= 450 || y < 0)
 		return true;
-	if (board[Math.floor(x / 60)][Math.floor(y / 45)] == 1)
+	let i = Math.floor(x / 60);
+	let j = Math.floor(y / 45);
+	let wall = board[i][j];
+	if (wall == 1)
 		return true;
 	return false;
 }
@@ -739,6 +755,9 @@ function touchCandy(){
 		changeScore(board[i][j]);
 		board[i][j] = 0;
 		countCandy++;
+		if (countCandy == ballsNumber){
+			won();
+		}
 	}
 }
 
@@ -763,12 +782,11 @@ function pacmanContact(x, y, width, heigh){
 }
 
 function initPacmanPosition(){
-	//choose random empty point for pacman at start of game that not in the edges of the bord
+	//choose random empty point for pacman at start of game that not in the edges of the bord.
 	let found = false;
 	let i = -1;
 	let j = -1;
 	while (!found){
-		position = [Math.floor(Math.random() * board.length), Math.floor(Math.random() * board[0].length)];
 		i = Math.floor(Math.random() * board.length);
 		j = Math.floor(Math.random() * board[0].length);
 		if (board[i][j] != 1 && (i != 0 && j != 0) && (i != board.length-1 && j != 0) && (i != board.length-1 && j != board[0].length-1) && (i != 0 && j != board[0].length-1))
@@ -776,6 +794,22 @@ function initPacmanPosition(){
 	}
 	pacX = i * 60 + 30;
 	pacY = j * 45 + 22;
+}
+
+function initMedicationPosition(difficulty){
+	//choose random empty point for the medication.
+	let found = false;
+	let i = -1;
+	let j = -1;
+	while (!found){
+		i = Math.floor(Math.random() * board.length);
+		j = Math.floor(Math.random() * board[0].length);
+		if (board[i][j] != 1)
+			found = true;
+	}
+	medicationX = i * 60 + 30;
+	medicationY = j * 45 + 22;
+	medicationTime = medicationTimeTolive * difficulty;
 }
 
 function initCherry(){
@@ -903,7 +937,28 @@ function changeGhostsLocations(){
 		let ghostY=ghostsPositions[j][1];
 		let availableDirectionsX = [];
 		let availableDirectionsY = [];
+		let demoPacX = pacX;
+		let demoPacY = pacY;		
+		let newDist = 1;
+		let minX = ghostX;
+		let minY = ghostY;
+		let bestDistanceX = 99999999999;
+		let bestDistanceY = 99999999999;
 
+		// make sure the ghost won't go together.
+		if (Math.random() >= 0.5)
+		{
+			if (j == 0)
+				demoPacX -= 30;
+			if (j == 1)
+				demoPacX += 30;
+			if (j == 2)
+				demoPacY -= 30;
+			if (j == 3)
+				demoPacY += 30;
+		}
+		
+		//check available directions.
 		if (!(checkWall(ghostX, ghostY-ghostSpeed) || checkWall(ghostX+ghostWidth, ghostY-ghostSpeed)))
 			availableDirectionsY.push("up");
 		if (!(checkWall(ghostX, ghostY+ghostSpeed+ghostHeigh) || checkWall(ghostX+ghostWidth, ghostY+ghostSpeed+ghostHeigh)))
@@ -913,23 +968,17 @@ function changeGhostsLocations(){
 		if (!(checkWall(ghostX+ghostSpeed+ghostWidth, ghostY) || checkWall(ghostX+ghostSpeed+ghostWidth, ghostY+ghostHeigh)))
 			availableDirectionsX.push("right");
 
-		let newDist = 1;
-		let minX = ghostX;
-		let minY = ghostY;
-		let bestDistanceX = 99999999999;
-		let bestDistanceY = 99999999999;
-
 		for(let i=0;i<availableDirectionsY.length;i++){
 			switch (availableDirectionsY[i]){
 				case "up":
-					newDist = Math.abs(ghostY-ghostSpeed-pacY);
+					newDist = Math.abs(ghostY-ghostSpeed-demoPacY);
 					if (newDist < bestDistanceY){
 						minY = ghostY - ghostSpeed;
 						bestDistanceY = newDist;
 					}
 					break;
 				case "down":
-					newDist = Math.abs(ghostY+ghostSpeed-pacY);
+					newDist = Math.abs(ghostY+ghostSpeed-demoPacY);
 					if (newDist < bestDistanceY){
 						minY = ghostY + ghostSpeed;
 						bestDistanceY = newDist;
@@ -943,14 +992,14 @@ function changeGhostsLocations(){
 		for(let i=0;i<availableDirectionsX.length;i++){
 			switch (availableDirectionsX[i]){
 				case "left":
-					newDist = Math.abs(ghostX-ghostSpeed-pacX);
+					newDist = Math.abs(ghostX-ghostSpeed-demoPacX);
 					if (newDist < bestDistanceX){
 						minX = ghostX - ghostSpeed;
 						bestDistanceX = newDist;
 					}
 					break;
 				case "right":
-					newDist = Math.abs(ghostX+ghostSpeed-pacX);
+					newDist = Math.abs(ghostX+ghostSpeed-demoPacX);
 					if (newDist < bestDistanceX){
 						minX = ghostX + ghostSpeed;
 						bestDistanceX = newDist;
@@ -963,10 +1012,11 @@ function changeGhostsLocations(){
 		ghostsPositions[j] = [minX, minY];
 		if (pacmanContact(minX, minY, ghostWidth, ghostHeigh)){
 			changeScore(-10);
-			decreaseLives();
+			changeLives(-1);
 			initPacmanPosition();
 			initGhostPositions();
 			initGhostsArr();
+			initMedicationPosition(medicationDifficulty);
 		}
 	}
 }
@@ -977,13 +1027,8 @@ function changeScore(s){
 	document.getElementById("scoreLabel").innerHTML = score;
 }
 
-function decreaseLives(){
-	lives--;
-	drawLives();
-}
-
-function increaseLives(){
-	lives++;
+function changeLives(num){
+	lives += num;
 	drawLives();
 }
 
@@ -994,6 +1039,12 @@ function drawLives(){
 		img.src="Img/live.png";
 		document.getElementById("livesDiv").appendChild(img);
 	}
+}
+
+//NEED TO CHANGE!!!!!!!!!!!!!!!!
+function won(){
+	stopTimer();
+	stopGamesIntervals();
 }
 
 function aboutScreen(){
